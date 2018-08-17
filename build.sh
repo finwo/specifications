85f61ec8781561c66e417a682586d57962ec0afb
Robin Bron <robin@finwo.nl>
2018-08-17 18:06:48 +0200

    Clearer build script

--- build.sh
+++ build.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
-# Update the .gitignore file
-curl -s https://www.gitignore.io/api/osx,linux,windows,intellij > .gitignore
+echo "Updating gitignore"
+curl -s https://www.gitignore.io/api/osx,linux,windows,intellij,composer > .gitignore
 echo /.gtm/ >> .gitignore
 echo /.idea/ >> .gitignore
+echo composer.lock >> .gitignore
+
+echo "Updating PHP dependencies"
+composer install &>/dev/null
+
+echo "(Re)building generated specs"
+php compile.php spec=0000
