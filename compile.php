49b521a681fdf475ecdf5cf2dab574a4f7e1d407
Robin Bron <robin@finwo.nl>
2018-08-17 18:07:22 +0200

    Working on the parser

--- compile.php
+++ compile.php
@@ -1,6 +1,7 @@
 <?php
 
 if(!defined('DS')) define('DS',DIRECTORY_SEPARATOR);
+if(!defined('EOL')) define('EOL',"\n");
 require(__DIR__.DS.'vendor'.DS.'autoload.php');
 $params = array();
 @array_shift($argv);
@@ -30,31 +31,14 @@ function flatten( $data, $prefix = '', &$output = [] ) {
 function unflatten( $flatArray, &$output = [] ) {
     static $accessor = null;
     $accessor = $accessor ?: new \Finwo\PropertyAccessor\PropertyAccessor(true);
+    if(is_string($flatArray)) {
+        $flatArray = str_replace(["\r\n","\r"],"\n",$flatArray);
+        $flatArray = array_filter(explode("\n",$flatArray));
+        $flatArray = array_map(function($line){return array_map('trim',explode(':',$line));},$flatArray);
+        $flatArray = array_map(function($kv){$kv[0]=strtolower($kv[0]);return $kv;},$flatArray);
+        $flatArray = array_reduce($flatArray,function($acc,$kv){$acc[$kv[0]]=$kv[1];return $acc;},[]);
+    }
     foreach ($flatArray as $key => $value ) $accessor->set($output,$key,$value,'.');
-    return $output;
-}
-
-function configFile( $file, &$output = [] ) {
-    $fd = null;
-    if(is_array($file)) $file = implode(DS,$file);
-    if(is_string($file) && file_exists($file)) { $fd = fopen($file,'r'); $file = null; }
-    if(is_string($file)) {
-        $fd = fopen('php://temp','r+');
-        fwrite($fd,$file);
-        fseek($fd,0);
-        $file = null;
-    }
-    if(!$fd) return null;
-    while(!feof($fd)) {                                  // Loop until we're out of file
-        $line = fgets($fd);                              // Fetch the line we're about to process
-        $line = @array_shift(str_getcsv($line,'#'));     // Strip comments
-        $line = trim($line);                             // Trim line (we don't care about whitespace)
-        if(strlen($line)==0) continue;                   // Blank line = skip
-        $line = array_map('trim',str_getcsv($line,':')); // Split key & value
-        if(count($line)!=2) continue;                    // Only 1 key & 1 value supported
-        $output[strtolower($line[0])]=$line[1];          // Flat keys
-    }
-    fclose($fd);
     return $output;
 }
 
@@ -64,27 +48,11 @@ function fcopy($src,$dst=null) {
     return $dst;
 }
 
-// Make sure we get the minimum params
-if(!isset($params['spec'])) perror('Spec missing from the parameters');
-$spec = $params['spec'];
-
-// Ensure the said spec has a src
-if(!file_exists('src'.DS.$spec.'.md')) perror('Given source does not exist');
-$filename = implode(DS,[__DIR__,'src',$spec.'.md']);
-
-// Fetch the file's headers
-$fd       = fopen($filename,'r+');
-$headers  = '';
-while(!feof($fd)) {
-    $line = trim(fgets($fd));
-    if(!$line) break;
-    $headers .= $line . "\n";
-}
-$headers = unflatten(configFile($headers));
-
 class Pipe {
     protected $processor = null;
     protected $next      = null;
+    protected $writable  = true;
+    protected $userdata  = null;
     public function __construct( $processor, Pipe $next = null ) {
         if(is_array($processor)) {
             $this->processor = array_shift($processor);
@@ -95,69 +63,120 @@ class Pipe {
         }
     }
     public function write( $chunk ) {
-        if(is_resource($this->processor)) return fwrite($this->processor,$chunk);
-        if(is_callable($this->processor)) return call_user_func($this->processor,$chunk,$this->next);
+        if(!$this->writable) return false;
+        if(is_resource($this->processor)&&$chunk) return fwrite($this->processor,$chunk);
+        if(is_callable($this->processor)) return call_user_func_array($this->processor,[$chunk,$this->next,&$this->userdata]);
         return false;
+    }
+    public function end( $chunk = null ) {
+        if(!is_null($chunk)) $this->write($chunk);
+        $this->write(null);
+        if(is_resource($this->processor)) $this->processor = fclose($this->processor);
+        if($this->next instanceof Pipe) $this->next->end();
     }
 }
 function normalize_newlines( $chunk, Pipe $next ) {
-    $next->write(str_replace(["\r\n","\r"],"\n",$chunk));
+    if(is_null($chunk)) return;
+    $next->write(str_replace(["\r\n","\r"],EOL,$chunk));
 }
 function inclusions( $chunk, Pipe $next ) {
-    if(substr($chunk,0,1)=='<') {
-        $fd = fopen('src'.DS.substr(trim($chunk),1).'.md','r');
-        while(!feof($fd)) $next->write(fgets($fd));
+    if(is_null($chunk)) return;
+    $indent = strlen(rtrim($chunk))-strlen(trim($chunk));
+    if(substr($chunk,$indent,1)=='<') {
+        $fd = fopen('src'.DS.substr(trim($chunk),1).'.spec','r');
+        while(!feof($fd)) $next->write(str_repeat(' ',$indent).fgets($fd));
         fclose($fd);
     } else {
         $next->write($chunk);
     }
 }
+function to_chars( $chunk, Pipe $next ) {
+    if(is_null($chunk)) return;
+    foreach (str_split($chunk) as $chr) $next->write($chr);
+}
+function group_symbols( $char, Pipe $next, &$data ) {
+    $data = $data ?: '';
+    switch($char) {
+        case '`':
+            $data .= $char;
+            break;
+        default:
+            if(strlen($data)) $next->write($data);
+            $data = '';
+            $next->write($char);
+    }
+}
+function group_unbreakables( $symbol, Pipe $next, &$data ) {
+    $data = $data ?: [];
+    $cnt  = count($data);
+    $top  = $cnt ? $data[$cnt-1] : null;
+    switch($symbol) {
+        case '```':
+        case '`':
+        case '"':
+        case "'":
+            if($cnt) {
+                if($top[0]==$symbol) {
+                    $me = array_pop($data);
+                    if($cnt==1) {
+                        $next->write($me[1].$symbol.EOL);
+                    } else {
+                        $data[$cnt-2][1] .= $me[1].$symbol;
+                    }
+                } else {
+                    $data[] = [$symbol,$symbol];
+                }
+            } else {
+                $data[] = [$symbol,$symbol];
+            }
+            break;
+        default:
+            if($cnt) {
+                $data[$cnt-1][1] .= $symbol;
+            } else {
+                $next->write($symbol.EOL);
+            }
+            break;
+    }
+}
+
+// Make sure we get the minimum params
+if(!isset($params['spec'])) perror('Spec missing from the parameters');
+$spec = $params['spec'];
+
+// Ensure the said spec has a src
+if(!file_exists('src'.DS.$spec.'.spec')) perror('Given source does not exist');
+$filename = implode(DS,[__DIR__,'src',$spec.'.spec']);
+
+// Fetch the file's headers
+$fd       = fopen($filename,'r+');
+$headers  = '';
+while(!feof($fd)) {
+    $line = trim(fgets($fd));
+    if(!$line) break;
+    $headers .= $line . "\n";
+}
+$headers = unflatten($headers);
 
 // Build the processing pipe
-$outfd = fopen('spec'.DS.'spec'.$spec.'.txt','c+');
-ftruncate($outfd,0);
-$pipe = new Pipe([
-    'normalize_newlines',
-    'inclusions',
-    $outfd,
-]);
+$pipes = [
+    "txt" => new Pipe([
+        'normalize_newlines',
+        'inclusions',
+        'to_chars',
+        'group_symbols',
+        'group_unbreakables',
+        fopen('spec'.DS.'spec'.$spec.'.txt','w+'),
+    ]),
+];
 
-// Run data through the pipe
-var_dump($pipe);
-while(!feof($fd)) $pipe->write(fgets($fd));
-var_dump($pipe);
+// Run data through the pipes
+while(!feof($fd)) {
+    $line = fgets($fd);
+    foreach ($pipes as $pipe)
+        $pipe->write($line);
+}
 
-//// Handle file inclusions
-//$temp_fd = fopen('php://temp','c+');
-//while(!feof($process_fd)) {
-//    $line = fgets($process_fd);
-//    if(substr($line,0,1)==='<') {
-//        $fd = fopen('src'.DS.trim(substr($line, 1)) . '.md', 'r');
-//        var_dump('src'.DS.trim(substr($line, 1)) . '.md');
-//        fcopy($fd, $temp_fd);
-//        fclose($fd);
-//    } else {
-//        fwrite($temp_fd,$line);
-//    }
-//}
-//
-//// Rewind again
-//fseek($process_fd,0);
-//ftruncate($process_fd,0);
-//fseek($temp_fd,0);
-//fcopy($temp_fd,$process_fd);
-//fseek($process_fd,0);
-//fseek($temp_fd,0);
-//ftruncate($temp_fd,0);
-//
-//
-//
-//
-//
-//echo stream_get_contents($process_fd);
-//
-//
-////var_dump($config);
-////var_dump($contents);
-//var_dump($headers);
-//var_dump($params);
+// Close the pipes
+foreach ($pipes as $pipe)
+    $pipe->end();
