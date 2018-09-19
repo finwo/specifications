d2d4b80d4867f0008ca64f58864a9c858fd7bf56
finwo <finwo@pm.me>
2018-09-19 15:28:09 +0200

    Still working on the unilicense

--- build.sh
+++ build.sh
@@ -1,23 +1,28 @@
 #!/usr/bin/env bash
 
-echo "Updating gitignore"
-curl -s https://www.gitignore.io/api/osx,linux,windows,intellij,composer > .gitignore
-echo /.gtm/ >> .gitignore
-echo /.idea/ >> .gitignore
-echo composer.lock >> .gitignore
-echo 'src/*/*.pdf' >> .gitignore
-echo '*.aux' >> .gitignore
-echo '*.log' >> .gitignore
-echo '*.toc' >> .gitignore
-
-echo "Updating PHP dependencies"
-composer install &>/dev/null
+#echo "Updating gitignore"
+#curl -s https://www.gitignore.io/api/osx,linux,windows,intellij,composer > .gitignore
+#echo /.gtm/ >> .gitignore
+#echo /.idea/ >> .gitignore
+#echo composer.lock >> .gitignore
+#echo 'src/*/*.pdf' >> .gitignore
+#echo '*.aux' >> .gitignore
+#echo '*.log' >> .gitignore
+#echo '*.toc' >> .gitignore
+#
+#echo "Updating PHP dependencies"
+#composer install &>/dev/null
 
 echo "(Re)building generated specs"
+ORGDIR=$(pwd)
 SPECDIR=$(pwd)/spec
-for d in $(find $(pwd)/src -mindepth 1 -maxdepth 1 -type d); do
-  cd $d
-  pdflatex content.tex && cp content.pdf ${SPECDIR}/$(basename $d).pdf
+cd src
+for f in $(find . -type f -regextype posix-egrep -regex '\./[0-9]{4}.*\.tex'); do
+  pdflatex ${f}
+  pdflatex ${f}
+  cp ${f%.*}.pdf ${SPECDIR}
+  rm -f *.pdf
+  rm -f *.aux
+  rm -f *.log
+  rm -f *.toc
 done
-#pdflatex -output-directory=src/0002 -aux-directory=aux src/0002/content.tex && cp src/0002/content.pdf spec/0002.pdf
-#pandoc -f latex -o spec/spec0002.pdf src/0002.tex
