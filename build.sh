54016ed9f3f612b41e62a9751c6493805b7cbb83
Robin Bron <finwo@pm.me>
2021-09-27 17:33:13 +0200

    Added markdown and hbs support in build script

--- build.sh
+++ build.sh
@@ -12,7 +12,10 @@ find ${ORGDIR} -maxdepth 1 -type d -regextype posix-egrep -regex '.*[0-9]{4}' | 
 
   # Read specification document data
   declare -A DATA
-  while IFS='=' read key value; do
+  while read line; do
+    key=${line%%=*}
+    value=${line#*=}
+    if [ -z "$key" ]; then continue; fi
     DATA["$key"]="$value"
   done <<< "$(tool/ini.sh ${spec}/data.ini)"
 
@@ -29,7 +32,7 @@ find ${ORGDIR} -maxdepth 1 -type d -regextype posix-egrep -regex '.*[0-9]{4}' | 
   fi
 
   # Check how to render the document itself
-  filename=$(echo ${spec}/document.*)
+  filename=$(ls ${spec}/document.* | head -1)
   case "${filename##*.}" in
     tex)
       pdflatex ${filename} >/dev/null
@@ -64,12 +67,23 @@ find ${ORGDIR} -maxdepth 1 -type d -regextype posix-egrep -regex '.*[0-9]{4}' | 
       # Remove rendered version
       rm ${filename}.rendered
       ;;
+    md)
+      tool/template.sh -c ${spec}/data.ini ${filename} | smu > ${filename}.html
+      htmldoc --webpage --textfont sans --size A4 -f ${SPECDIR}/${DATA[identifier]}.pdf ${filename}.html
+      rm ${filename}.html
+      ;;
+    hbs)
+      # Include render data
+      tool/template.sh -c ${spec}/data.ini ${filename} > ${filename}.html
+      htmldoc --webpage --textfont sans --size A4 -f ${SPECDIR}/${DATA[identifier]}.pdf ${filename}.html
+      rm ${filename}.html
+      ;;
     rendered)
       # Skip
       ;;
     *)
-      echo "Unknown document type: ${filename##*.}" 1>&2
-      exit 1
+      # echo "Unknown document type: ${filename##*.}" 1>&2
+      # exit 1
   esac
 done
 
